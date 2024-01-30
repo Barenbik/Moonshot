664e125ab76f0c3a666ce6a7fec54af9141ac989
Tony Sharples <tonysharples22@me.com>
2024-01-30 17:58:09 +0000

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Tony Sharples on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
