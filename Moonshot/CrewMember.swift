e310c764441f161943f720e63603c9198ab0a688
Tony Sharples <tonysharples22@me.com>
2024-02-14 15:40:51 +0000

    Create CrewMember class and get CrewView working

---
+++ Moonshot/CrewMember.swift
@@ -0,0 +1,8 @@
+//
+//  CrewMember.swift
+//  Moonshot
+//
+//  Created by Tony Sharples on 14/02/2024.
+//
+
+import Foundation
---
+++ Moonshot/CrewView.swift
@@ -0,0 +1,18 @@
+//
+//  CrewView.swift
+//  Moonshot
+//
+//  Created by Tony Sharples on 13/02/2024.
+//
+
+import SwiftUI
+
+struct CrewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CrewView()
+}
